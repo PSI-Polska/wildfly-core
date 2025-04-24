2d1e4820b63ebddf8a5c523fd8ae889f72e8ad5e
Darran Lofthouse <darran.lofthouse@jboss.com>
2025-04-24 13:12:22 +0100

    [WFCORE-7233] Remove unused AuthConfigFactory delegation code.

--- elytron/src/main/java/org/wildfly/extension/elytron/ElytronDefinition.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/ElytronDefinition.java
@@ -26,7 +26,6 @@ import static org.wildfly.extension.elytron.Capabilities.SSL_CONTEXT_CAPABILITY;
 import static org.wildfly.extension.elytron.CustomComponentDefinition.wrapFunction;
 import static org.wildfly.extension.elytron.ElytronExtension.isServerOrHostController;
 import static org.wildfly.extension.elytron.SecurityActions.doPrivileged;
-import static org.wildfly.extension.elytron._private.ElytronSubsystemMessages.ROOT_LOGGER;
 
 import java.security.PrivilegedAction;
 import java.security.Provider;
@@ -89,12 +88,10 @@ import org.wildfly.security.auth.server.EvidenceDecoder;
 import org.wildfly.security.auth.server.ModifiableSecurityRealm;
 import org.wildfly.security.auth.server.PrincipalDecoder;
 import org.wildfly.security.auth.server.RealmMapper;
-import org.wildfly.security.auth.server.SecurityDomain;
 import org.wildfly.security.auth.server.SecurityRealm;
 import org.wildfly.security.authz.PermissionMapper;
 import org.wildfly.security.authz.RoleDecoder;
 import org.wildfly.security.authz.RoleMapper;
-import org.wildfly.security.manager.WildFlySecurityManager;
 import org.wildfly.security.manager.action.ReadPropertyAction;
 
 /**
@@ -406,19 +403,6 @@ class ElytronDefinition extends SimpleResourceDefinition {
         });
     }
 
-    private static AuthConfigFactory getAuthConfigFactory() {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        try {
-            Thread.currentThread().setContextClassLoader(ElytronDefinition.class.getClassLoader());
-            return AuthConfigFactory.getFactory();
-        } catch (Exception e) {
-            ROOT_LOGGER.trace("Unable to load default AuthConfigFactory.", e);
-            return null;
-        } finally {
-            Thread.currentThread().setContextClassLoader(classLoader);
-        }
-    }
-
     private static SecurityPropertyService uninstallSecurityPropertyService(OperationContext context) {
         ServiceRegistry serviceRegistry = context.getServiceRegistry(true);
 
@@ -588,15 +572,4 @@ class ElytronDefinition extends SimpleResourceDefinition {
 
     }
 
-    private static final Supplier<Boolean> ALLOW_DELEGATION = new Supplier<Boolean>() {
-
-        @Override
-        public Boolean get() {
-            if (WildFlySecurityManager.isChecking()) {
-                return doPrivileged((PrivilegedAction<Boolean>) () -> SecurityDomain.getCurrent() == null);
-            } else {
-                return SecurityDomain.getCurrent() == null;
-            }
-        }
-    };
 }
