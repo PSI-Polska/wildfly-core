01765296993baf48f37ef2f2e5d64ace57c915ce
Darran Lofthouse <darran.lofthouse@jboss.com>
2025-09-22 12:40:54 +0100

    [WFCORE-7322] Record a jakarta-authorization capability if registration was successful.

--- elytron/src/main/java/org/wildfly/extension/elytron/ElytronDefinition.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/ElytronDefinition.java
@@ -11,6 +11,7 @@ import static org.jboss.as.server.security.VirtualDomainUtil.VIRTUAL_SECURITY_DO
 import static org.wildfly.extension.elytron.Capabilities.AUTHENTICATION_CONTEXT_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.ELYTRON_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.EVIDENCE_DECODER_RUNTIME_CAPABILITY;
+import static org.wildfly.extension.elytron.Capabilities.JAKARTA_AUTHORIZATION_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.MODIFIABLE_SECURITY_REALM_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.PERMISSION_MAPPER_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.PRINCIPAL_DECODER_RUNTIME_CAPABILITY;
@@ -91,7 +92,7 @@ import org.wildfly.security.auth.server.SecurityRealm;
 import org.wildfly.security.authz.PermissionMapper;
 import org.wildfly.security.authz.RoleDecoder;
 import org.wildfly.security.authz.RoleMapper;
-import org.wildfly.security.jakarta.authz.AuthorizarionRegistration;
+import org.wildfly.security.jakarta.authz.AuthorizationRegistration;
 import org.wildfly.security.manager.action.ReadPropertyAction;
 
 import jakarta.security.auth.message.config.AuthConfigFactory;
@@ -492,8 +493,13 @@ class ElytronDefinition extends SimpleResourceDefinition {
 
             if (context.isNormalServer()) {
 
-                if (!AuthorizarionRegistration.register()) {
-                    throw ROOT_LOGGER.unableToRegisterJakartaAuthorization();
+                try {
+                    if (AuthorizationRegistration.register()) {
+                        ROOT_LOGGER.trace("Jakarta Authorization Dynamically Registered.");
+                        context.registerCapability(JAKARTA_AUTHORIZATION_RUNTIME_CAPABILITY);
+                    }
+                } catch (SecurityException e) {
+                    throw ROOT_LOGGER.unableToRegisterJakartaAuthorization(e);
                 }
 
                 context.addStep(new AbstractDeploymentChainStep() {
